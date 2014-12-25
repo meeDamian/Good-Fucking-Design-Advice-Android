b8262a77c089bb4bc649a8a2d2e100157c9118b3
Damian Mee <damian.wasilewski@gmail.com>
2014-12-26 00:41:16 +0100

    common lib added

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Generate db from sql and save it to assets
-cat advices.sql | sqlite3 mobile/src/main/assets/advices.db
-
-# Copy db to wearable
-cp mobile/src/main/assets/advices.db wear/src/main/assets/advices.db
+# Generate db from sql and save it to shared assets
+cat advices.sql | sqlite3 common/src/main/assets/advices.db
