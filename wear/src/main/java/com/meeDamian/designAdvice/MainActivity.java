c86946b03739cbaabecc98a1f4fadf83fbb7c47c
Damian Mee <damian.wasilewski@gmail.com>
2014-12-26 03:17:36 +0100

    wear app MVP ready

--- wear/src/main/java/com/meeDamian/designAdvice/MainActivity.java
+++ wear/src/main/java/com/meeDamian/designAdvice/MainActivity.java
@@ -2,23 +2,33 @@ package com.meeDamian.designAdvice;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+import com.meedamian.common.Advice;
+import com.meedamian.common.MyDatabase;
 
 public class MainActivity extends Activity {
 
-    //private TextView mTextView;
+    private MyDatabase db;
+    private TextView advice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*
+
+        db = new MyDatabase(this);
+
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
+                advice = (TextView) stub.findViewById(R.id.advice);
+
+                Advice a = db.getNewAdvice();
+                advice.setText(a.getBody());
             }
         });
-        */
     }
 }
