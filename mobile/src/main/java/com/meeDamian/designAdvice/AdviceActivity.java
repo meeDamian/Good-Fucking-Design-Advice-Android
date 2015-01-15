98516da0cd2a6c9b04b21c4f247d5a5824802fdd
Damian Mee <damian.wasilewski@gmail.com>
2015-01-15 04:03:47 +0100

    WIP autoresize + (almost) share

--- mobile/src/main/java/com/meeDamian/designAdvice/AdviceActivity.java
+++ mobile/src/main/java/com/meeDamian/designAdvice/AdviceActivity.java
@@ -1,6 +1,8 @@
 package com.meeDamian.designAdvice;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
@@ -14,15 +16,27 @@ import android.widget.TextView;
 import com.meedamian.common.Advice;
 import com.meedamian.common.MyDatabase;
 
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
 public class AdviceActivity extends Activity implements ImageButton.OnClickListener {
 
     private MyDatabase db;
     private TextView advice;
     private TextView adviceId;
 
+    private Advice a;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+            .setDefaultFontPath("fonts/Roboto-Bold.ttf")
+            .setFontAttrId(R.attr.fontPath)
+            .build()
+        );
+
         setContentView(R.layout.activity_advice);
 
         db = new MyDatabase(this);
@@ -32,6 +46,14 @@ public class AdviceActivity extends Activity implements ImageButton.OnClickListe
 
         ImageButton nextAdvice = (ImageButton) findViewById(R.id.nextAdvice);
         nextAdvice.setOnClickListener(this);
+
+        ImageButton share = (ImageButton) findViewById(R.id.share);
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shareThat();
+            }
+        });
 
         Bundle extras = getIntent().getExtras();
         String id = extras != null
@@ -43,6 +65,11 @@ public class AdviceActivity extends Activity implements ImageButton.OnClickListe
         NotificationHelper.la(this);
 
         AlarmHelper.setAlarm(this, AlarmHelper.ALARM_HOUR);
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 
     @Override
@@ -59,19 +86,31 @@ public class AdviceActivity extends Activity implements ImageButton.OnClickListe
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(visibility);
 
-
         super.onResume();
     }
 
     private void setNewAdvice(@Nullable String id) {
-        Advice a = id != null
+        a = id != null
             ? db.getNewAdvice(id)
             : db.getNewAdvice();
 
-        adviceId.setText(a.getId() + ".");
+        adviceId.setText("#" + a.getId());
         SpannableString aoe = new SpannableString(a.getBody());
         aoe.setSpan(new ForegroundColorSpan(Color.rgb(248, 68, 68)), aoe.length()-1, aoe.length(), 0);
+
+        // NOTE uncomment once line height fix is found
+        //aoe.setSpan(new RelativeSizeSpan(1.5f), aoe.length()-1, aoe.length(), 0);
+
         advice.setText(aoe);
+    }
+
+    private void shareThat() {
+        Intent share = new Intent(Intent.ACTION_SEND);
+        share.setType("text/plain");
+        share.putExtra(Intent.EXTRA_SUBJECT, a.getBody());
+        share.putExtra(Intent.EXTRA_TITLE, a.getBody());
+        share.putExtra(Intent.EXTRA_TEXT, a.getUrl());
+        startActivity(Intent.createChooser(share, "Share Advice"));
     }
 
     @Override
