d96e1a9dc39e98dcbf9007c12a1de900b59955e8
Damian Mee <damian.wasilewski@gmail.com>
2015-03-14 01:20:46 +0700

    bla bla

--- prepare.sh
+++ prepare.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# 1. sort by line hash
+# 1. sort by line hash (deterministic)
 # 2. append with current line number
 # 3. sort back by IDs
 # 4. turn into SQL
 # 5. replace last comma with a semicolon
 # 6. save output to tmp file
 cat rawQuotes.txt \
-    | gsort -R \
+    | gsort -R --random-source=/dev/zero \
     | awk '{printf("%s, %d\n", $0,NR)}' \
     | gsort -h \
     | awk '{ printf("(%s),\n", $0 ) }' \
@@ -15,11 +15,11 @@ cat rawQuotes.txt \
     > tmp
 
 # replace quotes section in actual .sql file
-awk '                                                                                                                                     ✭ ✈ ✱
-    BEGIN       {p=1}
-    /^-- START/   {print;system("cat tmp");p=0}
-    /^-- END/     {p=1}
-    p' advices.sql > advices.sql
+lead='^-- START_QUOTES$'
+tail='^-- END_QUOTES$'
+sed -i '' -e "/$lead/,/$tail/{ /$lead/{p; r tmp
+}; /$tail/p; d
+}" advices.sql
 
 # clean up
 rm tmp
